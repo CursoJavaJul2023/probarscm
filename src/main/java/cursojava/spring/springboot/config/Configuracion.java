1f60ee0e7e0ebeb20dcff7319ea27b09f0bef7ce
feynman <feynman@localhost>
2023-08-07 14:01:06 +0200

    Añadido @EntityScan

--- src/main/java/cursojava/spring/springboot/config/Configuracion.java
+++ src/main/java/cursojava/spring/springboot/config/Configuracion.java
@@ -1,5 +1,6 @@
 package cursojava.spring.springboot.config;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -13,6 +14,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(
 	basePackages = {
 		"cursojava.jpahibernate.orm.modelocompras.repositorios"
+	}
+)
+@EntityScan(
+	basePackages = {
+		"cursojava.jpahibernate.orm.modelocompras.entidades",
+		"cursojava.jpahibernate.orm.modelocompras.dto"
 	}
 )
 public class Configuracion {
