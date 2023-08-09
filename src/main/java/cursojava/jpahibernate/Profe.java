a0c5ef2434a1c5c669ab227b1b2fb9cd61e88978
josema <jose@algo.es>
2023-08-09 14:00:55 +0200

    Intento de colision

--- src/main/java/cursojava/jpahibernate/Profe.java
+++ src/main/java/cursojava/jpahibernate/Profe.java
@@ -3,7 +3,7 @@ package cursojava.jpahibernate;
 public class Profe {
 	
 	public static void main(String[] args) {
-		System.out.println("Hola y adios");
+		System.out.println("Hola y hasta luego");
 	}
 
 }
